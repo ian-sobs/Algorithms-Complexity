3c6cdaeba99a46daf1dd491bd127f99e91106543
ian <ian.sobrecaray@gmail.com>
2024-09-26 01:46:41 +0800

    Changed the number of elements in the array and their values

--- tournament_sort_offline/main.c
+++ tournament_sort_offline/main.c
@@ -2,18 +2,18 @@
 #include "def.h"
 
 int main(){
-    int count = 12;
-    int arr[12] = {85, -62, 61, 90, -96, 1, 8, -5, 1, 0, -1, -2};
+    int count = 5;
+    int arr[5] = {1, 8, -5, 1, 0};
     int x;
-    for(x = 0; x < 12; ++x){
+    for(x = 0; x < count; ++x){
         printf("%d ", arr[x]);
     }
 
     printf("\n\n");
 
-    tournament_sort(arr, 12);
+    tournament_sort(arr, count);
 
-    for(x = 0; x < 12; ++x){
+    for(x = 0; x < count; ++x){
         printf("%d ", arr[x]);
     }
 

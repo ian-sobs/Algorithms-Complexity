518ebc98c6834c752e815bf81e31fb81fec082fd
ian <ian.sobrecaray@gmail.com>
2024-09-28 20:12:14 +0800

    fixed infinite loop problem

--- comb_sort/func.c
+++ comb_sort/func.c
@@ -5,7 +5,8 @@ void comb_sort(int arr[], int count){//pushing down
     int y, gap, temp;
     bool swapped = true;
 
-    for(gap = count / 1.3; gap != 1 || swapped; gap /= 1.3){
+    for(gap = count; gap != 1 || swapped; ){
+        gap /= 1.3;
         if(gap < 1) gap = 1;
         swapped = false;
         for(y = count - 1; y >= gap; --y){

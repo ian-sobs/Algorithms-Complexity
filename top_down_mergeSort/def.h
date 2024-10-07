c9219a6a2f736f971c35a0a12efd7de7abda6afe
ian <ian.sobrecaray@gmail.com>
2024-10-07 17:01:10 +0800

    Made the code cleaner

--- top_down_mergeSort/def.h
+++ top_down_mergeSort/def.h
@@ -1,2 +1,3 @@
 
 void top_down_mergeSort(int arr[], int left, int right);
+void merge(int arr[], int left, int right);
--- top_down_mergeSort/func.c
+++ top_down_mergeSort/func.c
@@ -3,45 +3,50 @@
 
 void top_down_mergeSort(int arr[], int left, int right){
     if(left < right){
-        int trav, travL, travR;
         int mid = (left + right) / 2; 
-        int arrLeftCount = mid - left + 1, 
-            *arrLeft = (int*) malloc(sizeof(int) * arrLeftCount);
-        int arrRightCount = right - mid, 
-            *arrRight = (int*) malloc(sizeof(int) * arrRightCount);
 
-        if(arrLeft != NULL && arrRight != NULL){
-            for(travL = 0, trav = left; trav <= mid; ++trav, ++travL){
-                arrLeft[travL] = arr[trav];
-            }        
-            for(travR = 0; trav <= right; ++trav, ++travR){
-                arrRight[travR] = arr[trav];
-            } 
+        top_down_mergeSort(arr, left, mid); // sort left sub-array
+        top_down_mergeSort(arr, mid + 1, right); // sort right sub-array
 
-            top_down_mergeSort(arrLeft, 0, arrLeftCount - 1); // sort left sub-array
-            top_down_mergeSort(arrRight, 0, arrRightCount - 1); // sort right sub-array
+        merge(arr, left, right); 
+    }
+}
 
-            // using the left and right sub-arrays to sort arr[]
-            for(travL = travR = 0, trav = left; travL < arrLeftCount && travR < arrRightCount; ++trav){
-                if(arrLeft[travL] > arrRight[travR]){
-                    arr[trav] = arrRight[travR];
-                    ++travR;
-                }
-                else{
-                    arr[trav] = arrLeft[travL];
-                    ++travL;
-                }
-            }
+void merge(int arr[], int left, int right){
+    int trav, travL, travR;
+    int mid = (left + right) / 2; 
+    int arrLeftCount = mid - left + 1, 
+        *arrLeft = (int*) malloc(sizeof(int) * arrLeftCount);
+    int arrRightCount = right - mid, 
+        *arrRight = (int*) malloc(sizeof(int) * arrRightCount);
 
-            for(; travL < arrLeftCount; ++travL, ++trav){
-                arr[trav] = arrLeft[travL];
-            }
-            for(; travR < arrRightCount; ++travR, ++trav){
+    if(arrLeft != NULL && arrRight != NULL){ 
+        for(travL = 0, trav = left; trav <= mid; ++trav, ++travL){
+            arrLeft[travL] = arr[trav];
+        }        
+        for(travR = 0; trav <= right; ++trav, ++travR){
+            arrRight[travR] = arr[trav];
+        } 
+
+        for(travL = travR = 0, trav = left; travL < arrLeftCount && travR < arrRightCount; ++trav){
+            if(arrLeft[travL] > arrRight[travR]){
                 arr[trav] = arrRight[travR];
+                ++travR;
             }
+            else{
+                arr[trav] = arrLeft[travL];
+                ++travL;
+            }
+        }
 
-            free(arrLeft);
-            free(arrRight);
-        }   
+        for(; travL < arrLeftCount; ++travL, ++trav){
+            arr[trav] = arrLeft[travL];
+        }
+        for(; travR < arrRightCount; ++travR, ++trav){
+            arr[trav] = arrRight[travR];
+        }
+
+        free(arrLeft);
+        free(arrRight);
     }
 }
